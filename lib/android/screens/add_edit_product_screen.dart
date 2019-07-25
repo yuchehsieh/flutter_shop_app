b7c394f50ed3bff4147d1ed199e2782f9ada570c
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-25 11:46:30 +0800

    learn: TextFormField, CupertinoTextField default take as much as width it can get, be careful working with Row, AND playaround with FocusNode also addListener to check if hasFocus (remember to remove it)

--- lib/android/screens/add_edit_product_screen.dart
+++ lib/android/screens/add_edit_product_screen.dart
@@ -11,13 +11,29 @@ class MaterialAddEditProduct extends StatefulWidget {
 class _MaterialAddEditProductState extends State<MaterialAddEditProduct> {
   final _priceFoucusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     _priceFoucusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _imageUrlController.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
@@ -55,6 +71,34 @@ class _MaterialAddEditProductState extends State<MaterialAddEditProduct> {
                   maxLines: 3,
                   keyboardType: TextInputType.multiline,
                 ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(top: 8, right: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey),
+                      ),
+                      child: _imageUrlController.text.isEmpty
+                          ? Text('Enter a URL')
+                          : FittedBox(
+                              child: Image.network(_imageUrlController.text),
+                              fit: BoxFit.cover,
+                            ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Image URL'),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        focusNode: _imageUrlFocusNode,
+                        controller: _imageUrlController,
+                      ),
+                    )
+                  ],
+                )
               ],
             ),
           ),
--- lib/ios/screens/add_edit_product_screen.dart
+++ lib/ios/screens/add_edit_product_screen.dart
@@ -10,13 +10,29 @@ class CupertinoAddEditProduct extends StatefulWidget {
 class _CupertinoAddEditProductState extends State<CupertinoAddEditProduct> {
   final _priceFoucusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     _priceFoucusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _imageUrlController.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
@@ -87,6 +103,53 @@ class _CupertinoAddEditProductState extends State<CupertinoAddEditProduct> {
                   maxLines: 3,
                   focusNode: _descriptionFocusNode,
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20.0,
+                    left: 20.0,
+                    right: 20.0,
+                    bottom: 10.0,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        width: 100,
+                        height: 100,
+                        margin: EdgeInsets.only(top: 8, right: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 1, color: CupertinoColors.inactiveGray),
+                        ),
+                        child: _imageUrlController.text.isEmpty
+                            ? Text(
+                                'Enter a URL',
+                                style: TextStyle(
+                                    color: CupertinoColors.inactiveGray),
+                              )
+                            : FittedBox(
+                                child: Image.network(_imageUrlController.text),
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                      Expanded(
+                        child: CupertinoTextField(
+                          padding: EdgeInsets.only(
+                            top: 10.0,
+                            left: 10.0,
+                            right: 0.0,
+                            bottom: 5.0,
+                          ),
+                          placeholder: 'Enter image url ...',
+                          keyboardType: TextInputType.url,
+                          textInputAction: TextInputAction.done,
+                          focusNode: _imageUrlFocusNode,
+                          controller: _imageUrlController,
+                        ),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
