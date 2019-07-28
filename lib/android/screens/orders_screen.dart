6522876b10f3f70e53b0078e048e26791dc8bdb3
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-28 17:32:57 +0800

    learn: VERY POWERFUL FutureBuilder, implement on both Material and Cupertino's OrderScreen, CupertinoOrderScreen extra trymeself add an error handling

--- lib/android/screens/orders_screen.dart
+++ lib/android/screens/orders_screen.dart
@@ -4,64 +4,48 @@ import 'package:shop_app/android/widgets/app_drawer.dart';
 import 'package:shop_app/android/widgets/order_item.dart';
 import 'package:shop_app/providers/orders.dart';
 
-class MaterialOrdersScreen extends StatefulWidget {
+class MaterialOrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _MaterialOrdersScreenState createState() => _MaterialOrdersScreenState();
-}
-
-class _MaterialOrdersScreenState extends State<MaterialOrdersScreen> {
-  bool _isLoading = false;
-
-  /* Using different approach */
-  /*
-   * With (listen: false), 
-   * could ignore the Future.delayed(Duration.zero)
-   */
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  // bool _isInit = true;
-  // @override
-  // void didChangeDependencies() {
-  //   if(_isInit) {
-  //     Provider.of<Orders>(context).fetchAndSetOrders().then(() {
-  //       ///
-  //     });
-
-  //   }
-  //   _isInit = false;
-  //   super.didChangeDependencies();
-  // }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    /*
+      // final orderData = Provider.of<Orders>(context);
+      If don't comment this out,
+      when FutureBuilder done, it'll triger re-build
+      and re-build will trigger FutureBuilder ...
+
+      So Avoid the inifinte-loop,
+      Simply wrap Widget with Consumer!
+    */
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Order'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (context, index) => MaterialOrderItem(
-                orderData.orders[index],
-              ),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your Order'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              if (dataSnapshot.error != null) {
+                // ...
+                // Do some error handling
+                return Text('An Error Occured');
+              } else {
+                return Consumer<Orders>(
+                  builder: (context, orderData, child) => ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (context, index) => MaterialOrderItem(
+                      orderData.orders[index],
+                    ),
+                  ),
+                );
+              }
+            }
+          },
+        ));
   }
 }
--- lib/ios/screens/order_screen.dart
+++ lib/ios/screens/order_screen.dart
@@ -3,55 +3,71 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/ios/widgets/order_item.dart';
 import 'package:shop_app/providers/orders.dart';
 
-class CupertinoOrderScreen extends StatefulWidget {
-  @override
-  _CupertinoOrderScreenState createState() => _CupertinoOrderScreenState();
-}
-
-class _CupertinoOrderScreenState extends State<CupertinoOrderScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
+class CupertinoOrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
 
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: const Text('My Order'),
       ),
       child: SafeArea(
-        child: _isLoading
-            ? Center(child: CupertinoActivityIndicator(radius: 10))
-            : CustomScrollView(
+          child: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (_, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CupertinoActivityIndicator(radius: 10));
+          } else {
+            // if (dataSnapshot.error != null) {
+            // ...
+            // Do some error handling
+            //
+            // Error Handling down there
+            // }
+            return Consumer<Orders>(
+              builder: (_, orderData, child) => CustomScrollView(
                 slivers: <Widget>[
                   CupertinoSliverRefreshControl(
                     onRefresh: () => orderData.fetchAndSetOrders(),
                   ),
                   SliverList(
                     delegate: SliverChildBuilderDelegate(
-                        (BuildContext context, int index) {
-                      return CupertinoOrderItem(
-                        orderData.orders[index],
-                      );
-                    }, childCount: orderData.orders.length),
+                      (BuildContext context, int index) {
+                        return (dataSnapshot.error != null)
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    dataSnapshot.error.toString(),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  SizedBox(height: 20),
+                                  Text(
+                                    'PULL TO REFRESH',
+                                    style: TextStyle(
+                                      color: CupertinoColors.activeGreen,
+                                    ),
+                                    // textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              )
+                            : CupertinoOrderItem(
+                                orderData.orders[index],
+                              );
+                      },
+                      childCount: (dataSnapshot.error != null)
+                          ? 1
+                          : orderData.orders.length,
+                    ),
                   )
                 ],
               ),
-      ),
+            );
+          }
+        },
+      )),
     );
   }
 }
