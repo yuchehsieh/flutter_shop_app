0d43c6b39b99d9d3cc8abf78d9eaf4922602fc0f
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-31 11:18:28 +0800

    learn: using sliver in MaterialProductDetail, SliverAppBar basically work on MaterialApp, could create the nice animation

--- lib/android/screens/prodcut_detail_screen.dart
+++ lib/android/screens/prodcut_detail_screen.dart
@@ -21,32 +21,28 @@ class MaterialProdcutDetailScreen extends StatelessWidget {
       title: Text(loadedProduct.title),
     );
     return Scaffold(
-      appBar: appBar,
-      body: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height -
-              appBar.preferredSize.height -
-              MediaQuery.of(context).padding.top -
-              MediaQuery.of(context).padding.bottom,
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 300,
-                width: double.infinity,
-                child: Hero(
-                  tag: loadedProduct.id,
-                  child: Image.network(
-                    loadedProduct.imageUrl,
-                    fit: BoxFit.cover,
-                  ),
+      // appBar: appBar,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
                 ),
               ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: Column(
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                const SizedBox(height: 10),
+                Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
@@ -92,11 +88,85 @@ class MaterialProdcutDetailScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
-              // AnimatedIcon(icon: AnimatedIcons.home_menu, progress: controller
-            ],
+                SizedBox(height: 800),
+              ],
+            ),
           ),
-        ),
+        ],
+        // child: Container(
+        //   width: MediaQuery.of(context).size.width,
+        //   height: MediaQuery.of(context).size.height -
+        //       appBar.preferredSize.height -
+        //       MediaQuery.of(context).padding.top -
+        //       MediaQuery.of(context).padding.bottom,
+        //   child: Column(
+        //     // mainAxisAlignment: MainAxisAlignment.end,
+        //     crossAxisAlignment: CrossAxisAlignment.center,
+        //     children: <Widget>[
+        //       Container(
+        //         height: 300,
+        //         width: double.infinity,
+        //         child: Hero(
+        //           tag: loadedProduct.id,
+        //           child: Image.network(
+        //             loadedProduct.imageUrl,
+        //             fit: BoxFit.cover,
+        //           ),
+        //         ),
+        //       ),
+        //       const SizedBox(height: 10),
+        //       Expanded(
+        //         child: Column(
+        //           mainAxisAlignment: MainAxisAlignment.center,
+        //           children: <Widget>[
+        //             Text(
+        //               '\$${loadedProduct.price}',
+        //               style: const TextStyle(color: Colors.grey, fontSize: 20),
+        //             ),
+        //             const SizedBox(height: 10),
+        //             Container(
+        //               width: double.infinity,
+        //               padding: const EdgeInsets.symmetric(horizontal: 10),
+        //               child: Text(
+        //                 '${loadedProduct.description}',
+        //                 textAlign: TextAlign.center,
+        //                 softWrap: true,
+        //               ),
+        //             ),
+        //             PopupMenuButton(
+        //               // onCanceled: ,
+        //               offset: Offset(-30, 50),
+        //               // enabled: false,
+        //               // elevation: 5,
+        //               // initialValue: FilterOptions.Favorites,
+        //               onSelected: (FilterOptions selectedValue) {
+        //                 // setState(() {
+        //                 //   if (selectedValue == FilterOptions.Favorites) {
+        //                 //     _showOnlyFavorites = true;
+        //                 //   } else {
+        //                 //     _showOnlyFavorites = false;
+        //                 //   }
+        //                 // });
+        //               },
+        //               itemBuilder: (_) => [
+        //                 PopupMenuItem(
+        //                   child: Text('Only Favorites'),
+        //                   value: FilterOptions.Favorites,
+        //                 ),
+        //                 PopupMenuItem(
+        //                   child: Text('Show All'),
+        //                   value: FilterOptions.All,
+        //                 ),
+        //               ],
+        //               icon: Icon(Icons.more_vert),
+        //             ),
+        //           ],
+        //         ),
+        //       )
+        //       // AnimatedIcon(icon: AnimatedIcons.home_menu, progress: controller
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
