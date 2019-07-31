6b6f12b83bc3ada9a7c7ed0e179b096340ad29fb
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-31 14:57:20 +0800

    try successed: Cupertino's order-item that button-arrow animation successfully rotate

--- lib/ios/widgets/order_item.dart
+++ lib/ios/widgets/order_item.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop_app/providers/orders.dart';
 
+enum ExpandedState { Expanded, Collapsed }
+
 class CupertinoOrderItem extends StatefulWidget {
   final OrderItem order;
 
@@ -14,73 +16,99 @@ class CupertinoOrderItem extends StatefulWidget {
   _CupertinoOrderItemState createState() => _CupertinoOrderItemState();
 }
 
-class _CupertinoOrderItemState extends State<CupertinoOrderItem> {
-  bool _isExpanded = false;
+class _CupertinoOrderItemState extends State<CupertinoOrderItem>
+    with SingleTickerProviderStateMixin {
+  ExpandedState _expandedState;
+  AnimationController _controller;
+  Animation<double> _rotateAnimation;
+
+  @override
+  initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _rotateAnimation = Tween<double>(
+      begin: 0.0,
+      end: 0.5,
+    ).animate(
+      CurvedAnimation(
+        curve: Curves.easeIn,
+        parent: _controller,
+      ),
+    );
+
+    super.initState();
+  }
 
   _toggleCollpased() {
-    setState(() {
-      _isExpanded = !_isExpanded;
-    });
+    if (_expandedState == ExpandedState.Expanded) {
+      setState(() {
+        _expandedState = ExpandedState.Collapsed;
+      });
+      _controller.reverse();
+    } else {
+      setState(() {
+        _expandedState = ExpandedState.Expanded;
+      });
+      _controller.forward();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      height: _isExpanded
-          ? min(widget.order.products.length * 20.0 + 110.0, 200)
-          : 95,
-      duration: Duration(milliseconds: 300),
-      curve: Curves.easeIn,
-      child: Card(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
-              subtitle: Text(
-                DateFormat('dd-MM-yyy hh:mm').format(widget.order.dateTime),
-              ),
-              trailing: CupertinoButton(
-                child: _isExpanded
-                    ? Icon(CupertinoIcons.up_arrow)
-                    : Icon(CupertinoIcons.down_arrow),
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('\$${widget.order.amount.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd-MM-yyy hh:mm').format(widget.order.dateTime),
+            ),
+            trailing: RotationTransition(
+              turns: _rotateAnimation,
+              alignment: Alignment.center,
+              child: CupertinoButton(
+                child: Icon(CupertinoIcons.down_arrow),
                 onPressed: _toggleCollpased,
               ),
             ),
-            AnimatedContainer(
-              height: _isExpanded
-                  ? min(widget.order.products.length * 20.0 + 10.0, 180)
-                  : 0,
-              duration: Duration(milliseconds: 300),
-              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-              child: ListView(
-                children: widget.order.products
-                    .map(
-                      (prod) => Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            prod.title,
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+          ),
+          AnimatedContainer(
+            height: _expandedState == ExpandedState.Expanded
+                ? min(widget.order.products.length * 30.0 + 10.0, 180)
+                : 0,
+            duration: Duration(milliseconds: 300),
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            child: ListView(
+              children: widget.order.products
+                  .map(
+                    (prod) => Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          prod.title,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                           ),
-                          Text(
-                            '${prod.quantity}x \$${prod.price}',
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                    .toList(),
-              ),
-            )
-          ],
-        ),
+                        ),
+                        Text(
+                          '${prod.quantity}x \$${prod.price}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
+          )
+        ],
       ),
     );
   }
